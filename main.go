bc76b6751db81eb90f4316e0d5e2030c631160e1
yuleihua <huayulei_2003@hotmail.com>
2018-03-24 20:11:41 +0800

    add files

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type KafkaServer struct {
+	Addrs   []string `toml:"addrs"`
+	Topics  []string `toml:"topics"`
+	Timeout int      `toml:"timeout"`
+}
+
+var (
+	topics   string
+	hosts    string
+	dir      string
+	loglevel int
+)
+
+func init() {
+	flag.StringVar(&topics, "t", "venus", "topic setting")
+	flag.StringVar(&hosts, "h", "127.0.0.1:9092", "broker setting")
+	flag.IntVar(&loglevel, "l", 5, "log level")
+	flag.StringVar(&dir, "d", "./kdata", "output directory setting")
+}
+
+func main() {
+	flag.Parse()
+
+	// setting logger
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors:   true,
+		TimestampFormat: "2006/01/02-15:04:05.000",
+	})
+	log.SetLevel(log.Level(loglevel))
+
+	addrList := strings.Split(hosts, ",")
+	tpList := strings.Split(topics, ",")
+	vs := &KafkaServer{
+		Addrs:   addrList,
+		Topics:  tpList,
+		Timeout: 200,
+	}
+
+	log.Infof("%v", vs)
+	// handle
+	if err := Setup(vs, dir); err != nil {
+		log.Fatalf("Setup error:%v", err)
+	}
+
+	if err := Run(); err != nil {
+		log.Errorf("%v", err)
+	}
+
+	if err := Shutdown(); err != nil {
+		log.Errorf("shutdown error:%v", err)
+	}
+
+	log.Error("shutting down end")
+}
---
+++ server.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
+)
+
+var wkctx WorkerInfo
+
+type WorkerInfo struct {
+	consumer   sarama.Consumer
+	interval   time.Duration
+	tickerTime time.Duration
+	tps        []string
+	wg         sync.WaitGroup
+	lock       sync.Mutex
+	dir        string
+}
+
+func Setup(sk *KafkaServer, dir string) error {
+	if sk == nil {
+		return errors.New("invalid parameter")
+	}
+
+	conf := sarama.NewConfig()
+	conf.Net.DialTimeout = time.Duration(sk.Timeout) * time.Second
+	consumer, err := sarama.NewConsumer(sk.Addrs, conf)
+	if err != nil {
+		log.Fatalf("Kafka newConsumer error, addr:%v, error:%v")
+	}
+	wkctx.consumer = consumer
+	wkctx.dir = dir
+	wkctx.tps = sk.Topics
+	wkctx.interval = 200 * time.Millisecond
+	log.Infof("server initiation end")
+	return nil
+}
+
+func readHandle(topic string, pidx int32) {
+	defer wkctx.wg.Done()
+
+	file, err := os.OpenFile(fmt.Sprintf("%v/%v_%v", wkctx.dir, topic, pidx), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("open file failed", err.Error())
+	}
+	defer file.Close()
+
+	pc, err := wkctx.consumer.ConsumePartition(topic, pidx, sarama.OffsetOldest)
+	if err != nil {
+		log.Fatalf("Kafka ConsumePartition error, topic:%v, error:%v", topic, err)
+	}
+	defer pc.Close()
+
+	log.Infof("reader worker topic[%s] start", topic)
+	count := 0
+
+	for {
+		select {
+		case msg := <-pc.Messages():
+			//log.Infof("Partition:%d, Offset:%d, Key:%s, Value:%s", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+			if topic != msg.Topic {
+				log.Errorf("topic is not match")
+				continue
+			}
+			log.Infof("Receive msg,%v:%v", string(msg.Key), string(msg.Value))
+			file.WriteString(string(msg.Value))
+			file.WriteString("\n")
+		}
+	}
+	log.Warnf("topic[%v:%v:%d] stop", topic, pidx, count)
+}
+
+func Run() error {
+	for _, t := range wkctx.tps {
+		if t != "" {
+			tpList, err := wkctx.consumer.Partitions(t)
+			if err != nil {
+				log.Fatalf("Failed to get topic(%s) list of partitions:%v", t, err)
+			}
+			log.Infof("get topic(%s) list of partitions:%v", t, tpList)
+			for pidx := range tpList {
+				go readHandle(t, int32(pidx))
+				wkctx.wg.Add(1)
+			}
+		}
+	}
+	return nil
+}
+
+func Shutdown() error {
+	wkctx.wg.Wait()
+
+	if wkctx.consumer != nil {
+		wkctx.consumer.Close()
+	}
+	log.Warnf("server shutdown end")
+	return nil
+}
